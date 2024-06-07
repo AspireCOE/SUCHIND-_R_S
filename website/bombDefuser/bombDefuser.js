f5a59c002204538841031928400683c86689c175
susi <rssuchind@gmail.com>
2024-06-07 11:23:34 +0530

    bombDefuser

--- website/bombDefuser/bombDefuser.js
+++ website/bombDefuser/bombDefuser.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 let timerEl = document.getElementById("timer");
 let userTxtEl = document.getElementById("defuser");
@@ -20,27 +19,4 @@ userTxtEl.addEventListener('keydown', function(event) {
         timerEl.textContent = "YOU DID IT WELL";
         clearInterval(intervalId);
     }
-=======
-
-let timerEl = document.getElementById("timer");
-let userTxtEl = document.getElementById("defuser");
-
-let countdown = 10;
-
-let intervalId = setInterval(function() {
-    countdown = countdown - 1;
-    timerEl.textContent = countdown;
-    if (countdown === 0) {
-        timerEl.textContent = "BOOM";
-        clearInterval(intervalId);
-    }
-}, 1000);
-
-userTxtEl.addEventListener('keydown', function(event) {
-    let userEnteredTxt = userTxtEl.value;
-    if (event.key === "Enter" && userEnteredTxt === "defuse" && userEnteredTxt !== 0) {
-        timerEl.textContent = "YOU DID IT WELL";
-        clearInterval(intervalId);
-    }
->>>>>>> b8065e3 (ASPIRE)
 });
